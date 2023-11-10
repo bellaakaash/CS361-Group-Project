97b014f37ef4942cab950170d7370f8a2a9d5950
bellaakaash <bellaak@oregonstate.edu>
2023-11-10 09:49:35 -0800

    Added sample data to database tables

--- database/ddl.sql
+++ database/ddl.sql
@@ -117,3 +117,69 @@ CREATE TABLE IF NOT EXISTS recipe_media (
     media_url TEXT NOT NULL,
     FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE
 );
+
+-- Insert sample data into 'users'
+INSERT INTO users (username, email, password_hash) VALUES
+('john_doe', 'john@example.com', 'hashedpassword123'),
+('jane_smith', 'jane@example.com', 'anotherhashedpass');
+
+-- Insert sample data into 'recipes'
+INSERT INTO recipes (user_id, title, cuisine_type, preparation_time, servings, instructions) VALUES
+(1, 'Spaghetti Carbonara', 'Italian', 30, 4, 'Boil pasta, fry pancetta, mix with eggs and cheese'),
+(1, 'Classic Cheeseburger', 'American', 25, 2, 'Grill beef patties, place cheese on top, serve with buns and condiments'),
+(2, 'Vegetable Stir Fry', 'Chinese', 20, 3, 'Stir fry vegetables with soy sauce and garlic, serve with rice');
+
+-- Insert sample data into 'ingredients'
+INSERT INTO ingredients (name, quantity, unit) VALUES
+('Spaghetti', 500, 'g'),
+('Pancetta', 100, 'g'),
+('Parmesan Cheese', 50, 'g'),
+('Ground Beef', 300, 'g'),
+('Cheddar Cheese', 100, 'g'),
+('Mixed Vegetables', 450, 'g'),
+('Soy Sauce', 50, 'ml');
+
+-- Insert sample data into 'recipe_ingredients'
+INSERT INTO recipe_ingredients (recipe_id, ingredient_id, quantity, unit) VALUES
+(1, 1, 500, 'g'),
+(1, 2, 100, 'g'),
+(1, 3, 50, 'g'),
+(2, 4, 300, 'g'),
+(2, 5, 100, 'g'),
+(3, 6, 450, 'g'),
+(3, 7, 50, 'ml');
+
+-- Insert sample data into 'meal_plans'
+INSERT INTO meal_plans (user_id, date, meal_type, recipe_id) VALUES
+(1, '2023-11-01', 'Dinner', 1),
+(2, '2023-11-02', 'Lunch', 3);
+
+-- Insert sample data into 'inventory_items'
+INSERT INTO inventory_items (user_id, ingredient_id, expiry_date, in_stock_quantity) VALUES
+(1, 1, '2023-12-01', 2),
+(2, 6, '2023-11-15', 1);
+
+-- Insert sample data into 'shopping_list'
+INSERT INTO shopping_list (user_id, ingredient_id, required_quantity) VALUES
+(1, 2, 200),
+(2, 5, 150);
+
+-- Insert sample data into 'recipe_reviews'
+INSERT INTO recipe_reviews (recipe_id, user_id, rating, review) VALUES
+(1, 2, 5, 'Delicious classic recipe with a twist.'),
+(2, 1, 4, 'Tasty, but could use more spice.');
+
+-- Insert sample data into 'nutrition_info'
+INSERT INTO nutrition_info (recipe_id, calories, protein, carbohydrates, fat, sodium, sugar) VALUES
+(1, 400, 10, 50, 10, 500, 5),
+(2, 600, 30, 40, 20, 700, 10);
+
+-- Insert sample data into 'user_favorite_recipes'
+INSERT INTO user_favorite_recipes (user_id, recipe_id) VALUES
+(1, 2),
+(2, 1);
+
+-- -- Insert sample data into 'recipe_media'
+-- INSERT INTO recipe_media (recipe_id, media_type, media_url) VALUES
+-- (1, 'Image', 'http://example.com/media/carbonara.jpg'),
+-- (2, 'Image', 'http://example.com/media/cheeseburger.jpg');
